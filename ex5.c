5a4a01ee21aa74b88ed4d52dc360cd29ff94d95e
Suhas Bhairav <suhasbhairav@gmail.com>
2016-01-18 22:46:28 +0100

    ex 5

---
+++ ex5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* This is a comment. */
+
+int main(int argc, char *argv[]){
+
+    int distance = 100;
+    printf("You are %d miles away.\n", distance);
+
+
+    return 0;
+
+}
