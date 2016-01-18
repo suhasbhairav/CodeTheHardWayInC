4a440a725e89ef450443da8c5f345ff638c0b163
Suhas Bhairav <suhasbhairav@gmail.com>
2016-01-18 22:38:12 +0100

    Few puts...

--- ex1.c
+++ ex1.c
@@ -3,7 +3,9 @@
 int main(int argc, char *argv[]){
 
     puts("Hello World.");
-    
+    puts("Coding in c after a long time.");
+    puts("This looks very good.");
+    puts("Lean C the hard way.");    
     return 0;
 
 }
