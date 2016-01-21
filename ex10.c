8f88e934907c69046cc572c3b95b7896c8dcaf3c
Suhas Bhairav <suhasbhairav@gmail.com>
2016-01-21 23:21:08 +0100

    Ex 10

--- ex10.c
+++ ex10.c
@@ -7,8 +7,16 @@ int main(int argc, char*argv[]){
         printf("arg %d: %s", i, argv[i]);
     }
 
+    char * states[] = {
+        "California", "Oregon",
+        "Washington", "Texas"
+    };
+    int num_states = 4;
 
+    for(i=0;i<num_states;i++){
+        printf("state %d: %s\n", i, states[i]);
 
+    }
     return 0;
 
 }
