694214ad3af078ad6a54c4549357cc36bee67d50
Suhas Bhairav <suhasbhairav@gmail.com>
2016-02-02 23:06:26 +0100

    Ex 17

---
+++ ex17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    int num = 0;
+    printf("Enter a number:\n");
+    scanf("%d", &num);
+    printf("You entered:%d",num);
+
+
+    return 0;
+}
