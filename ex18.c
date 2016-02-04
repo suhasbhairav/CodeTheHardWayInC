d508eb77a365aa02205216ff06b81fa60c87ec1f
Suhas Bhairav <suhasbhairav@gmail.com>
2016-02-04 22:54:39 +0100

    Typedef example

---
+++ ex18.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+typedef int integer;
+typedef double DoubleDataType;
+int main(int argc, char *argv[]){
+
+    integer count =100;
+    DoubleDataType dVal = 1.203;
+    printf("Count:%d\n", count);
+    printf("DoubleDataType:%f\n", dVal);
+    return 0;
+}
