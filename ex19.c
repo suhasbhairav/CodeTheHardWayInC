c7545860ec17fe915059db9e7fd2f585358a5dda
Suhas Bhairav <suhasbhairav@gmail.com>
2016-02-04 23:14:44 +0100

    Signed and Unsigned

---
+++ ex19.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char *argv[]){
+   unsigned int val = 3125442;
+   long valLong = 12456321778963114;
+   signed int signedInt = -12544;
+   
+   printf("Unsigned: %d\n",val);
+   printf("Long:%ld\n", valLong);
+   printf("Signed:%d\n", signedInt);
+
+
+
+    return 0;
+}
